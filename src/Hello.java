1e8b3294f25e77c6706201816881f43715a4b1f7
dougbrunos <dougbrunos@gmail.com>
2025-02-25 19:58:18 -0300

    novo metodo para portugues

--- src/Hello.java
+++ src/Hello.java
@@ -1,10 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
         ingles();
+        portugues();
     }
     
     public static void ingles() {
-        System.out.println("Hello, World!");
+        System.out.println("Hello World!");
+    }
+
+    public static void portugues() {
+        System.out.println("Olá Mundo!");
     }
 
 }
